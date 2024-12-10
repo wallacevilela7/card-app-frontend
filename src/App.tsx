8e711ff0b42e7b5fdcab78a01d2c11b1bac2e69b
Wallace Vilela <wallace.vilela9@gmail.com>
2024-12-10 16:33:57 -0300

    Create app

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import { Card } from './components/card/card';
+import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
+
+function App() {
+
+  const { data } = useFoodData();
+
+  return (
+    < div className = "container" >
+      <h1>Cardápio</h1>
+      <div className="card-grid">
+        {data?.map(foodData =>
+          <Card
+            price={foodData.price}
+            title={foodData.title}
+            image={foodData.image}
+          />
+        )}
+      </div>
+    </div >
+  )
+}
+
+export default App
